51661d7033c8fc991f6119a7376c6fbcdde28c25
albert9052 <aabb9052@gmail.com>
2022-01-18 10:35:29 +0000

    1. Changed a OAuth key.

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ app
   })
   .use(GAuth, {
     clientId:
-      "317495594650-nn5m556dfgm30dhperr4h2apva3lcp5d.apps.googleusercontent.com",
+	  "317495594650-racsmtup6d4bd9ur8esrkarfulqch1fl.apps.googleusercontent.com"
   })
   .use(VueAxios, axios)
   .component("Calendar", Calendar)
